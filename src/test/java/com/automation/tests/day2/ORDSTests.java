fecaec4b0f2311ec47898638624283adb6d828c5
cristi <luticr@gmail.com>
2020-05-19 19:17:41 -0400

    added dependencies

--- src/test/java/com/automation/tests/day2/ORDSTests.java
+++ src/test/java/com/automation/tests/day2/ORDSTests.java
@@ -4,6 +4,7 @@ package com.automation.tests.day2;
 
 
 import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -39,6 +40,49 @@ public class ORDSTests {
          * when() - to specify type of HTTP request: get, put, post, delete, patch, head, etc...
          * then() - to verify response, perform assertions
          */
+
+    }
+
+    @Test
+    @DisplayName("Get employee under specific ID")
+    public void getOneEmployee() {
+        //in URL we can specify path and query parameters
+        //path parameters are used to retrieve specific resource: for example 1 employee not all of them
+        //{id} - path variable, that will be replace with a value after comma
+        //after when() we specify HTTP request type/method/verb
+        //The path parameters. E.g. if path is "/book/{hotelId}/{roomNumber}" you can do <code>get("/book/{hotelName}/{roomNumber}", "Hotels R Us", 22);</code>.
+        Response response = given().
+                                 baseUri(BASE_URL).
+                            when().get("/employees/{id}",100).prettyPeek();
+
+        // how we verify response? - use assertions
+
+        response.then().statusCode(200); // to verify that status is 200
+
+        int statusCode = response.statusCode(); // to save status code in variable
+
+        Assertions.assertEquals(200, statusCode);
+
+
+    }
+
+    /*
+when our test passes  we prove it from html reports ?
+it depends from company to company.
+pdf reports -  excel report -  html reports can be used.
+HTML reports is most common way - it is easy to read easy to generate
+ */
+
+    @Test
+    @DisplayName("Get list of all countries and verify that status code is 200")
+    public void getAllCountries() {
+        given().
+                baseUri(BASE_URL).
+                when().
+                get("/countries/").prettyPeek().
+                then().
+                statusCode(200);
+
 
     }
 
